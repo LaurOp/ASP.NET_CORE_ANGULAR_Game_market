bc0024e983d309e1f0ff370060999e8108e14899
Laur <45435659+RookerStyle@users.noreply.github.com>
2022-01-19 01:53:12 +0200

    migrations

---
+++ Migrations/20220116175708_toate_tabelele.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Proiect.Migrations
+{
+    public partial class toate_tabelele : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Creatori",
+                table: "Creatori");
+
+            migrationBuilder.RenameTable(
+                name: "Creatori",
+                newName: "Creators");
+
+            migrationBuilder.AddColumn<int>(
+                name: "NumberOfEmployees",
+                table: "Creators",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Creators",
+                table: "Creators",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "DLCs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    About = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<float>(type: "real", nullable: false),
+                    IsSkin = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DLCs", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Games",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Price = table.Column<float>(type: "real", nullable: false),
+                    MinimumAge = table.Column<int>(type: "int", nullable: false),
+                    Purchases = table.Column<int>(type: "int", nullable: false),
+                    Grade = table.Column<float>(type: "real", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Games", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Genres",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ForKids = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genres", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Reviews",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Author = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Text = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    likes = table.Column<int>(type: "int", nullable: false),
+                    dislikes = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Reviews", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DLCs");
+
+            migrationBuilder.DropTable(
+                name: "Games");
+
+            migrationBuilder.DropTable(
+                name: "Genres");
+
+            migrationBuilder.DropTable(
+                name: "Reviews");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Creators",
+                table: "Creators");
+
+            migrationBuilder.DropColumn(
+                name: "NumberOfEmployees",
+                table: "Creators");
+
+            migrationBuilder.RenameTable(
+                name: "Creators",
+                newName: "Creatori");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Creatori",
+                table: "Creatori",
+                column: "Id");
+        }
+    }
+}
